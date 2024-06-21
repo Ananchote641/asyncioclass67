2d26efc498a22935ad6990856f41f75815f4f210
Ananchote641 <160345697+Ananchote641@users.noreply.github.com>
2024-06-21 11:35:44 +0700

    thread

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,1 +1,18 @@
 # running a function in another thread
+from time import sleep, ctime
+from threading import Thread
+
+#a custom funtion tat blocks for a moment
+def task():
+    #block for a moment
+    sleep(1)
+    # display a message
+    print(f'{ctime()}this is from another thread')
+
+# create a thread
+thread = Thread(target=task)
+#run the thread
+thread.start()
+#wait for the thread of finish
+print(f'{ctime()}Waiting for the thread...')
+thread.join()
--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,20 @@
 # extending the Thread class
+from threading import Thread
+from time import sleep, ctime
+
+# custom thread class
+class CustomThread(Thread):
+    # override the run function
+    def run(self):
+        # block for a moment
+        sleep(1)
+        # display a message
+        print(f'{ctime()} This is coming from another thread')
+
+# create the thread
+thread = CustomThread()
+# start the thread
+thread.start()
+# wait for the thread to finish
+print(f'{ctime()} Waiting for the thread to finish')
+thread.join()
